bd9ae821e6d230b716eb1c38f97854bedb5a7b14
bakhadir <bohodirziyodaliyev@gmail.com>
2023-04-23 16:23:44 +0500

    cors

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ dotenv.config()
 const PORT = process.env.PORT || 5001
 
 const app = express()
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // Update with the origin that you need to allow
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.json())
 app.use(cors());
 app.use(jobRouter)
