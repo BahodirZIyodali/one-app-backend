2ad8ef9138d2852496296080ad8bc89690d67413
bakhadir <bohodirziyodaliyev@gmail.com>
2023-04-23 11:28:01 +0500

    ok

--- ctrAndRouter/vacancies.js
+++ ctrAndRouter/vacancies.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const { readFile } = require("../utils/fs");
 const router = express.Router();
+const jobs = readFile("vacancies.json");
 
 const getVacancies = (_, res) => {
-  const jobs = readFile("vacancies.json");
   return res.status(200).json(jobs);
+};
+const getVacancy = (req, res) => {
+  const { position } = req.params;
+  const getOne = jobs.find((j) => j.position === position);
+  return res.status(200).json(getOne);
 };
 
 router.get("/vacancies", getVacancies);
-
+router.get("/vacancies/:position", getVacancy);
 module.exports = router;
