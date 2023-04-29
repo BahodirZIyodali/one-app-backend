407a52c87ad927a2fe95f12631378ad4cb46e369
Bahodir_Ziyodaliyev <113345966+BahodirZIyodali@users.noreply.github.com>
2023-04-29 08:48:08 +0500

    Update server.js

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(function(_, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
+   
   });
   
 app.use(express.json())
